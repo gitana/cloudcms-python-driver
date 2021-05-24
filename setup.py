ef34d0b01dd714731c5cd3e053c10cd077bb0cd9
mwhitman <mwhitman95@gmail.com>
2021-05-24 10:28:35 -0400

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='cloudcms',
-    version='1.1.0',
+    version='1.1.1',
     author='Michael Whitman',
     author_email='michael.whitman@cloudcms.com',
     description='Cloud CMS Python Driver',
